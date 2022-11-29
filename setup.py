0d8a5b74c6411b3b6140f6f3ff6d33b62089e3df
alexey_naidiuk <alexeynaidiuk@gmail.com>
2022-11-29 17:47:10 +0200

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='trading_bot',
+    version='0.0.1',
+    packages=['module'],
+    url='',
+    license='',
+    author='punk',
+    author_email='alexeynaidiuk@gmailcom',
+    description='trading bot for binance'
+)
